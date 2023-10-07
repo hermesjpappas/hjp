e7813337c6737522e0b9089579d9d538731ecb2a
Hermes J. Pappas <hjp@hjp.gr>
2023-10-07 13:53:34 +0300

    Fix typo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <nav class="absolute top-0 right-0 flex items-center w-full h-12 bg-white shadow-md">
     <span class="pl-4 font-serif text-xl"><RouterLink to="/">hjp</RouterLink></span>
     <ul class="flex items-center justify-end w-full gap-8 pr-4 font-medium">
-      <li><RouterLink to="/"></RouterLink>cv</li>
+      <li><RouterLink to="/">cv</RouterLink></li>
       <li><RouterLink to="/">code</RouterLink></li>
       <li><RouterLink to="/">my story</RouterLink></li>
       <li><RouterLink to="/">contact</RouterLink></li>
