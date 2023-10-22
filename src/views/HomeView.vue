ea22e76f12a7b9933c1c5b1e816d92c3ebacfa4e
Hermes J. Pappas <hjp@hjp.gr>
2023-10-22 20:43:25 +0300

    Fix spacing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,10 @@
   <main class="flex flex-col items-center justify-center w-full h-full font-serif">
     <h1 class="text-4xl xs:text-5xl lg:text-7xl">Hello!</h1>
     <h1 class="text-4xl xs:text-5xl lg:text-7xl">I'm Hermes.</h1>
-    <h2 class="mt-8 font-sans text-xl lg:mt-10 sm:text-3xl lg:text-5xl">Nice to meet you.</h2>
+    <h2 class="mt-8 font-sans text-xl xs:text-2xl lg:mt-10 sm:text-3xl lg:text-5xl">Nice to meet you.</h2>
 
     <div
-      class="flex items-center justify-center h-12 gap-4 mt-2 text-xl text-gray-600 md:mt-10 lg:mt-12 xs:gap-6 md:gap-8 lg:gap-12 xs:text-2xl md:text-3xl lg:text-4xl"
+      class="flex items-center justify-center h-12 gap-4 mt-6 text-xl text-gray-600 sm:mt-8 9 md:mt-10 lg:mt-12 xs:gap-6 md:gap-8 lg:gap-12 xs:text-2xl md:text-3xl lg:text-4xl"
     >
       <a href="https://github.com/hermesjpappas" target="_blank" class="w-8 transition-all duration-500 hover:text-black md:hover:text-4xl lg:hover:text-5xl"
         ><font-awesome-icon icon="fa-brands fa-github"
