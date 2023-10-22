30f991af761b43888417def494aaea16981da01f
Hermes J. Pappas <hjp@hjp.gr>
2023-10-22 17:20:27 +0300

    Turn back to hash until figuring out how to resolve history mode on server side

--- src/router/index.ts
+++ src/router/index.ts
@@ -26,7 +26,8 @@ const router = createRouter({
       path: '/story',
       name: 'story',
       component: StoryView
-    }
+    },
+    { path: '/:pathMatch(.*)*', name: 'not-found', component: HomeView }
   ]
 })
 
