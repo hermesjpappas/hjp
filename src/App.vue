a2fc22e6bc00c3c774f5bad8126b3b419011d95c
Hermes J. Pappas <hjp@hjp.gr>
2023-10-22 15:39:04 +0300

    Add scroll to top functionality

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue'
+function scrollTop() {
+  document.getElementById('app').scrollIntoView()
+}
 </script>
 
 <template>
   <div class="flex flex-col w-screen h-screen bg-gray-100">
     <NavBar />
     <main class="flex-1 overflow-y-auto sm:overflow-y-scroll top-20 xs:top-14">
-     <RouterView class="animate-fade-up"/> 
+      <transition @before-enter="scrollTop" mode="out-in" appear>
+        <RouterView class="animate-fade-up" />
+      </transition>
     </main>
   </div>
 </template>
@@ -18,10 +23,10 @@ a {
 }
 
 a:hover {
-  @apply underline text-gray-600
+  @apply underline text-gray-600;
 }
 
 .flag {
-  @apply w-24 h-14 xs:w-28 xs:h-16 md:w-32 md:h-20 rounded-md border-2 border-gray-700
+  @apply w-24 h-14 xs:w-28 xs:h-16 md:w-32 md:h-20 rounded-md border-2 border-gray-700;
 }
 </style>
